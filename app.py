d9350e3e8f6c439d5d1d1f2b56a1c28226d53edf
Tawab Baah <tawabbaah35@gmail.com>
2024-08-30 16:20:11 +0000

    Version 1

---
+++ app.py
@@ -0,0 +1,10 @@
+from ALX import backend_Engineer
+
+BE = backend_Engineer
+if me in BE:
+  del me.girls()
+  pubg = undifine
+  headache = True
+  sleepless_night = True
+else:
+  print("Bro you ")
